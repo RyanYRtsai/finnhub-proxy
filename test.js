69925ed09c680e20ede4ec89370a96d7392e5d75
RyanYRtsai <yoyo770411@gmail.com>
2025-08-15 11:52:53 +0800

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-// /api/test.js
-export default function handler(request, response) {
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.status(200).json({ 
-    message: "Hello from the Vercel test API!",
-    timestamp: new Date().toISOString() 
-  });
-}
